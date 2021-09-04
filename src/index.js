41e2aedf9d2893b826954678b131714d6089d2d8
Bernitafebri <bernitafebrihatin@students.undip.ac.id>
2021-09-04 21:55:31 +0700

    Modul 2 kel 10 selesai

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import CardList from "./components/CardList";
+import Quotes from "./components/Quotes";
+
+import Gambar from "./assets/images/ber.jpg";
+
+const quotes ="Disaat payung teduh ku dengar , disitu lah tugas-tugas terbabat abis";
+const newQuotes = "tes test tes";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <CardList 
+      name="Bernita Febrihatin" 
+      nim="21120118120023" 
+      kelompok="10" 
+      isNameBold 
+      image={Gambar}/>
+    <Quotes author="Orang" quotes={quotes} />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
